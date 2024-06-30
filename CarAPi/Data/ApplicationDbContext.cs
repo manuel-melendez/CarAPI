36c4b9aebfee3f0fde8d6bb788b228465a741720
manuel-melendez <mannymelendez1213@gmail.com>
2024-06-30 18:33:55 -0400

    first commit

---
+++ CarAPi/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using CarAPi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarAPi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>().HasData(
+                new Car
+                {
+                    Id = 1,
+                    Make = "Nissan",
+                    Model = "Rouge",
+                    Year = 2012,
+                    Mileage = 10000
+                });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ CarAPi/Migrations/20240630013207_init.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarAPi.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Make = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Mileage = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cars",
+                columns: new[] { "Id", "Make", "Mileage", "Model", "Year" },
+                values: new object[] { 1, "Nissan", 10000, "Rouge", 2012 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
